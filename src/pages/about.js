01fe90f775866b9ef6c4d33ebf1439708404ffad
Rocio <arias.roocio@gmail.com>
2021-06-19 15:52:48 +0200

    first commit gatsby project

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+    return (
+        <Layout>
+        <div>
+            <h1>About page</h1>
+            <p>Cillum sit dolor amet mollit cupidatat commodo enim commodo mollit mollit mollit laboris aliquip cillum. </p>
+            <p>Fugiat dolore do quis duis.</p>
+            <p>Cupidatat officia dolor minim nulla incididunt laborum irure et dolor exercitation.
+            </p>
+        </div>
+        </Layout>
+    )
+}
