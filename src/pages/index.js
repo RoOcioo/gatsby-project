65c2932a179bc2db1be545e4a18c3775dd864214
Rocio <arias.roocio@gmail.com>
2021-06-21 17:30:02 +0200

    gatsby proj

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
+import { Link } from 'gatsby'
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-    <section>
+    <section className={styles.header}>
       <div>
         <h2>Design</h2>
         <h3>Develop & Deploy</h3>
         <p>UX designer & web developer</p>
+        <Link className={styles.btn} to='/projects'>My Portfolio Projects</Link>
       </div>
+      <img src="/holi.jpg" alt="" style={{ maxWidth: '80%'}}/>
     </section>
     </Layout>
   )
